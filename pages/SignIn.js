6de2657c4d1d15233648f79a7db744bf85472e5d
Thomas Selden <thomasselden42@gmail.com>
2022-03-09 15:45:27 -0500

    added route to chat page and onclick event to send user to chat screen from sign in screen

--- pages/SignIn.js
+++ pages/SignIn.js
@@ -1,33 +1,37 @@
 import styles from "../styles/Home.module.css";
-import { Form } from "react-bootstrap";
-import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
-import logo from "../public/logo.jpg";
+import { Form, Row, Col} from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import logo from "./logo.jpg";
+
+
+
 
 const SignIn = () => {
   return (
     <div className={styles.SignIn}>
       <Form>
         <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-          <Form.Label column sm="2">
+          <Form.Label column sm="2" className="BoldText">
             Email
           </Form.Label>
           <Col sm="10">
             <Form.Control plaintext defaultValue="email@example.com" />
           </Col>
         </Form.Group>
-
         <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-          <Form.Label column sm="2">
+          <Form.Label column sm="2" >
             Password
           </Form.Label>
           <Col sm="10">
             <Form.Control type="password" placeholder="Password" />
           </Col>
         </Form.Group>
+        <Button variant="primary" href="/chat" onClick={''}>
+          Sign In
+        </Button>{" "}
       </Form>
       <div>
-        <img src={logo} />
+        <img src={logo}/>
       </div>
     </div>
   );
